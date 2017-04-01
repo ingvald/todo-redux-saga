e3cfcb12dcac4df35fcfa3f2ca7558f7f516dbb9
Ingvald Skaug <ingvald+github@gmail.com>
2017-04-01 18:22:31 +0200

    adjusted to this fork's dev db

--- src/core/firebase/config.js
+++ src/core/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCUll5AyYba1XL8NDYKZ51RGt90KofQo6c',
-  authDomain: 'todo-redux-saga.firebaseapp.com',
-  databaseURL: 'https://todo-redux-saga.firebaseio.com',
-  storageBucket: 'todo-redux-saga.appspot.com'
+  apiKey: 'AIzaSyCNnea5IF4gDjVpUiZsWGV50fxMPia3ps0',
+  authDomain: 'todo-redux-saga-d3ff3.firebaseapp.com',
+  databaseURL: 'https://todo-redux-saga-d3ff3.firebaseio.com',
+  storageBucket: 'todo-redux-saga-d3ff3.appspot.com'
 };
